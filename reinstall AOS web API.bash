3d2e4eb19e710cacfc0a38466b6a1dac94ab95e2
orhaimwerthaim <orhaimwerthaim@users.noreply.github.com>
2023-01-19 13:35:41 +0200

    Add files via upload

---
+++ reinstall AOS web API.bash
@@ -0,0 +1,24 @@
+#!/bin/bash
+#https://matt.might.net/articles/bash-by-example/
+#install VSCode from https://code.visualstudio.com/docs/?dv=linux64_deb
+
+
+
+echo 'reinstall the AOS'
+cd ~/AOS
+
+
+yes|rm -r ~/AOS/AOS-WebAPI
+
+echo 'Web API: Downloading code'
+git clone https://github.com/orhaimwerthaim/AOS-WebAPI.git
+echo 'Web API: Downloaded'
+
+echo 'build the Web-API'
+cd ~/AOS/AOS-WebAPI
+dotnet build WebApiCSharp.csproj
+
+ 
+
+dotnet build ~/AOS/AOS-WebAPI/WebApiCSharp.csproj /property:GenerateFullPaths=true /consoleloggerparameters:NoSummary
+
